4ee245b59bdbcda9bc8bab60436b058dd2a1c62e
tbreslein <tommy.breslein@protonmail.com>
2025-07-02 08:07:48 +0200

    add GPLv2 license

--- build.zig
+++ build.zig
@@ -1,3 +1,7 @@
+// Copyright (c) 2025
+// Author: Tommy Breslein (github.com/tbreslein)
+// License: GPL-2.0
+
 const std = @import("std");
 
 const examples = [_][]const u8{
--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,7 @@
+// Copyright (c) 2025
+// Author: Tommy Breslein (github.com/tbreslein)
+// License: GPL-2.0
+
 //! By convention, root.zig is the root source file when making a library. If
 //! you are making an executable, the convention is to delete this file and
 //! start with main.zig instead.
